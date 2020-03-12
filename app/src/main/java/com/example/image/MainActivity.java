0984e59141b33c1e2476c0a4da1ea65f5d5edd14
Martin Freire <martin.vallejo01@gmail.com>
2020-03-12 09:49:15 -0500

    Main Activity: Override onOptionItemSelected

--- app/src/main/java/com/example/image/MainActivity.java
+++ app/src/main/java/com/example/image/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     Toolbar toolbar;
 
@@ -22,5 +26,14 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.favorite:
+                Log.d(LOG_TAG, "Fav icon tapped");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
